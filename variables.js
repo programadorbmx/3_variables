a5bf63191fe06c361b703b7b49bb080001935e39
= <=>
2025-08-23 17:25:11 -0500

    practica de variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const firstName = "abel";
+const favoriteColor = "red";
+let currentLocation = "Guarne";
+let mood = "happy";
+mood = "learning";
+console.log(firstName);
+console.log(favoriteColor);
+console.log(currentLocation);
+console.log(mood);    
