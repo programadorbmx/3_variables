d93e0aa72020ffac41408783eb56d81a6e05450c
= <=>
2025-11-05 11:41:05 -0500

    aqui aprendi sobre declaracion y asignacion de constantes

--- variables.js
+++ variables.js
@@ -7,6 +7,12 @@ cajita=null
 console.log(cajita)
 
 cajita=3
-console.log(cajita)
+console.log(cajita);
 
-//
+//aqui aprendi sobre declaracion y asignacion de constantes
+const caja="abel";
+console.log(caja)
+caja="alicia"
+console.log(caja)
+//en la 2 asignacion me boto error porque no se puede meter nuevamente un valor en una constante
+
