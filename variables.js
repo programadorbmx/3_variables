1db3d53ba38ed4c307752c582522a369603022a0
= <=>
2025-11-05 11:28:35 -0500

    aqui aprendi sobre declaracion y asignacion de variables

--- variables.js
+++ variables.js
@@ -1,9 +1,12 @@
-const firstName = "abel";
-const favoriteColor = "red";
-let currentLocation = "Guarne";
-let mood = "happy";
-mood = "learning";
-console.log(firstName);
-console.log(favoriteColor);
-console.log(currentLocation);
-console.log(mood);    
+//aqui aprendi sobre declaracion y asignacion de variables
+let cajita;
+cajita="abel"
+console.log(cajita)
+
+cajita=null
+console.log(cajita)
+
+cajita=3
+console.log(cajita)
+
+//
