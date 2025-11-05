2716ada3c4f49af7135c3dff3115502e1ad7c1c8
= <=>
2025-11-05 18:08:28 -0500

    aprendi mucho hoy

--- variables.js
+++ variables.js
@@ -10,9 +10,47 @@ cajita=3
 console.log(cajita);
 
 //aqui aprendi sobre declaracion y asignacion de constantes
-const caja="abel";
+const carro="abel";
 console.log(caja)
 caja="alicia"
 console.log(caja)
 //en la 2 asignacion me boto error porque no se puede meter nuevamente un valor en una constante
 
+
+
+let nombre="nombre"
+console.log (nombre)
+
+let edad=7
+console.log (7)
+
+let ciudad="medellin"
+console.log(medellin)
+
+let hermana="alicia"
+console.log(alicia)
+
+let mama="teo"
+console.log(teo)
+
+let papa="mao"
+console.log (mao)
+
+let deporte="bmx"
+console.log(bmx)
+
+let mes="novienbre"
+console.log(novienbre)
+
+let juguete="legos y mi f150 raptor"
+console.log("legos y mi f150 raptor")
+
+let numero=492
+console.log(492)
+
+
+const caja="abel";
+console.log(carro)
+caja="caja"
+console.log(carro)
+const carro="tron"
